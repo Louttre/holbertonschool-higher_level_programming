ba6ea09bced44728a0ff081ce656ccb65067a017
root <seth.rueda@gmail.com>
2024-05-06 14:02:27 +0000

    respect pycodestyle

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented programming"):str.find("object-oriented programming") + len("object-oriented programming")]
+str = str[39:60] + ' ' + "with" + ' ' + "Python"
 print(str)
