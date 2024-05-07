75acbcfff26951c9d2e8405302423e01387d4b00
root <seth.rueda@mail.com>
2024-05-07 11:57:10 +0000

    add 3-print_alphabt.ty

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113
-        i++
+i = 97
+while (i < 123):
+    if i == 101 or i == 113:
+        i += 1
     print("{}".format(chr(i)), end='')
+    i +=1
