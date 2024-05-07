c4d15d40ebbf3dcda7c1a1e60926c254b63bf524
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-07 12:08:49 +0200

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,18 +5,19 @@ witness = 0
 if number < 0:
     number *= -1
     witness = 1
-if number % 10 < 6 and number % 10 != 0:
+d = number % 10
+if d < 6 and d != 0:
     if witness:
-        print(f"Last digit of -{number} is -{number % 10} and is less than 6 and not 0")
+        print(f"Last digit of -{number} is -{d} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {d} and is less than 6 and not 0")
 elif number % 10 > 5:
     if witness:
-        print(f"Last digit of -{number} is -{number % 10} and is less than 6 and not 0")
+        print(f"Last digit of -{number} is -{d} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {number % 10} and is greater than 5")
+        print(f"Last digit of {number} is {d} and is greater than 5")
 else:
     if witness:
-        print(f"Last digit of -{number} is -{number % 10} and is less than 6 and not 0")
+        print(f"Last digit of -{number} is -{d} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {number % 10} and is 0")
+        print(f"Last digit of {number} is {d} and is 0")
