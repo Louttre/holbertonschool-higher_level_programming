8cc2e33c40397c86127c4e83de1fd33ac4440c85
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-07 12:03:22 +0200

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -19,4 +19,4 @@ else:
     if witness:
         print(f"Last digit of -{number} is -{number % 10} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {number % 10} and is greater than 5")
+        print(f"Last digit of {number} is {number % 10} and is 0")
