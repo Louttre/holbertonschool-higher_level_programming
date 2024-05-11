dd4fd82c8fdc195f4efc8b8a554a0ed2b21bd4a7
root <seth.rueda@gmail.com>
2024-05-11 16:16:39 +0000

    add many exercices

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def pow(a, b):
+    result = 1
+    if b == 1:
+        return a
+    if b == 0:
+        return result
+    if b > 0:    
+        for i in range(b):
+            result *= a
+    else:
+        for i in range(-b):
+            result /= a
+    return result
+
