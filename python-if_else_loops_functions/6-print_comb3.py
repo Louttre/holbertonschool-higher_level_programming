f6c3be1437cf690d61b6a3715e610b2a90a53b48
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-11 17:16:57 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-    for j in range (0, 10):
+for i in range(0, 10):
+    for j in range(0, 10):
         if j > i:
             print("{}".format(i), end='')
             if i < 8:
