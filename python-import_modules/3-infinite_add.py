08c4e61887de431794b52c5719f574f3f6ae6b7a
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-11 19:07:37 +0200

    Create 3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    result = 0
+    if len(argv) == 1:
+        return result
+    else:
+        for i in range(1, len(argv) - 1):
+            result += argv[i]
+    return result
