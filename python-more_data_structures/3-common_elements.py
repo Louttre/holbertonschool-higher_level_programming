adcdba8485d41f5b0b8e60c2277c00776481a41f
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 15:18:34 +0200

    Update 3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    intersection_set = set.intersection(set1, set2)
+    intersection_set = set.intersection(set_1, set_2)
     return intersection_set
