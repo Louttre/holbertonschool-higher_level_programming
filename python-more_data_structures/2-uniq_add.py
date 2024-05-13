c6d0bf9efbc88be690e1ba621ed0c3414774004b
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 15:06:05 +0200

    Create 2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    res = 0
+    wit = 0
+    for i in my_list:
+        wit = 0
+        for j in my_list:
+            wit = 1
+        if wit = 1:
+            continue
+        else:
+            res += i
