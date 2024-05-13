ad7e688f175a442c297476e199067a0bea4605d5
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 16:16:47 +0200

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,7 @@ def roman_to_int(roman_string):
         'M': 1000
     }
     res = 0
-    if not roman_string or len(roman_string) == 0:
+    if not isinstance(roman_string, str) or not roman_string:
         return res
     for i in range(len(roman_string)):
         if i + 1 == len(roman_string) or roman_to_integer[roman_string[i]] >= roman_to_integer[roman_string[i + 1]]:
