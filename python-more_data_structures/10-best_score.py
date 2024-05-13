b4baa449de6797a5c67f91d73dc0d5e8235bc428
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 15:52:44 +0200

    Create 10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max = 0
+    for key, value in a_dictionary.items():
+        if best_key == 0 or value > a_dictionary[max]:
+            max = key
+    return max
