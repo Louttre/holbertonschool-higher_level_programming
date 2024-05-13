5cfdbce5e13bebb6f6b84ec4fb9d8fa60fa61b65
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 13:56:03 +0200

    Create 10-divisible_by_2.py

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    new_list[len(my_list)]
+    for idx, i in enumerate(my_list):
+        if i % 2 == 0:
+            new_list[idx] = True
+        else:
+            new_list[idx] = False
+    return new_list        
