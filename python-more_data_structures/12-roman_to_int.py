8cf80f8f569a0da4abd2ef92e03298b70a12adb7
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 16:19:20 +0200

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,7 +13,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return res
     for i in range(len(roman_string)):
-        if i + 1 == len(roman_string) or tr[roman_string[i]] >= tr[roman_string[i + 1]]:
+        if (i + 1 == len(roman_string) or
+                tr[roman_string[i]] >= tr[roman_string[i + 1]]):
             res += tr[roman_string[i]]
         else:
             res -= tr[roman_string[i]]
