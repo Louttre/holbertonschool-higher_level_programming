e0ff0ac70ad4395db19d1affe9bdafa8e76036f2
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 14:53:10 +0200

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square(a):
     return a * a
+
+
 def square_matrix_simple(matrix=[]):
     new_matrix = [list(map(square, row)) for row in matrix]
     return list(new_matrix)
