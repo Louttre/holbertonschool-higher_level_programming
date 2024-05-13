db469a4ef1094abc345ebc340b9ea2612a6778bb
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 15:55:18 +0200

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(a_dictionary):
         return None
     max = 0
     for key, value in a_dictionary.items():
-        if best_key == 0 or value > a_dictionary[max]:
+        if max == 0 or value > a_dictionary[max]:
             max = key
     return max
