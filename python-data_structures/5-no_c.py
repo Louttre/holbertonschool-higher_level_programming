7055fc8da30e93d80ae1f64d7e1baf3f0dde29f2
Louttre <seth.rueda@gmail.com>
2024-05-13 11:00:16 +0000

    add 5-no-c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_str = ""
     for letter in my_string:
         if ord(letter) == 99 or ord(letter) == 67:
-            letter = ''
+            new_str += ''
+        else:
+            new_str += letter
+    return new_str
