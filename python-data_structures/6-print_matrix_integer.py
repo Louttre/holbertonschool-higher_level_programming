8f119e65a45c485feab3614314f80c6c3ec59555
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 13:05:17 +0200

    Create 6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix[]:
+        for j in matrix[[]]:
+            if j < len(matrix[i]):
+                print("{}".format(matrix[i][j]), end=' ')
+            else:
+                print("{}".format(matrix[i][j]), end='\n')
