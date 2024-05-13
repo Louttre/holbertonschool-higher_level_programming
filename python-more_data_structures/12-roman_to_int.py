05f08dce5f4e3b9dfb5ab74b56e61137862cc2c2
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-13 16:08:34 +0200

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-roman_to_integer = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
-res = 0
-if not roman_string or len(roman_string) == 0:
+    roman_to_integer = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    res = 0
+    if not roman_string or len(roman_string) == 0:
+        return res
+    for letter in roman_string:
+        for key, value in roman_to_integer.items():  # Use items() method to iterate over key-value pairs
+            if letter == key:
+                res += value
     return res
-for letter in roman_string:
-    for key, value in roman_to_integer.items():
-        if letter == key:
-            res += value
-return res
