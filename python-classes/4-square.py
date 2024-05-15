40b8110c8690d98bde6ca1007644751726495bf9
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-15 14:55:44 +0200

    Create 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A class representing a square."""
+    def __init__(self, size=0):
+        """Initialize a square with the given size.
+
+        Args:
+            size (int, optional): The size of each side of the square.
+                Defaults to 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
+    @property
+    def size(self):
+        return self._size
+    @setter
+    def size(self, value):
+        self._size = value
