0331fbd339877618ed6fa4759796b20bf6b9d53b
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-15 15:14:59 +0200

    Update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -42,8 +42,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
+            for i in range(self.__size - 1):
                 print('#', end='')
-                for j in range(self.__size):
+                for j in range(self.__size - 1):
                     print('#', end='')
                 print()
