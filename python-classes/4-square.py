d758063b9abf9ffc06411faabe36bb194375bd91
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-15 15:07:48 +0200

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,7 +19,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
+
     def area(self):
         """Calculate the area of the square.
 
@@ -27,11 +27,11 @@ class Square:
             int: The area of the square.
         """
         return self.__size * self.__size
-        
+
     @property
     def size(self):
         return self.__size
-        
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
