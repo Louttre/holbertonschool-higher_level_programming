d2308033cec99bbbb3eab21dbdd03a4936b21bbc
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-20 14:58:28 +0200

    Create 5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+    """
+    Print the text with 2 new lines after each occurrence of '.', '?', and ':'.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    formatted_text = ""
+    for char in text:
+        formatted_text += char
+        if char in ['.', '?', ':']:
+            formatted_text += '\n\n'
+    print(formatted_text.strip())
