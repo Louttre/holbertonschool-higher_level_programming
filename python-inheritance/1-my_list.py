7d6f6ef4747ebb98c369befba9bc05059f18f5f9
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 16:09:57 +0200

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,5 +8,4 @@ class MyList(list):
     def print_sorted(self):
         """Print the list elements in ascending order."""
 
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
