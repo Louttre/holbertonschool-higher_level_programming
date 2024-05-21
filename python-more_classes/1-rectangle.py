2a322b25f77fc29b30579bb5ca679128449586a1
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 12:10:13 +0200

    Update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle instance.
-        
+
         Args:
             width (int): The width of the rectangle (default is 0).
             height (int): The height of the rectangle (default is 0).
@@ -29,27 +29,27 @@ class Rectangle:
     @property
     def width(self):
         """Get the width of the rectangle."""
-        
+
         return self.__width
 
-    @width.setter    
+    @width.setter
     def width(self, value):
         """Set the width of the rectangle.
-        
+
         Args:
             value (int): The new width value.
-        
+
         Raises:
             TypeError: If the width is not an integer.
             ValueError: If the width is less than 0.
-        """   
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property    
+    @property
     def height(self):
         """Get the height of the rectangle"""
         return self.__height
@@ -57,15 +57,15 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height of the rectangle.
-        
+
         Args:
             value (int): The new height value.
-        
+
         Raises:
             TypeError: If the height is not an integer.
             ValueError: If the height is less than 0.
         """
-        
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
