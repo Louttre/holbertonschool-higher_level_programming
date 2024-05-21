888f7d03110df3f5a84d84f187ba822308218487
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 17:37:36 +0200

    Create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class definition for BaseGeometry"""
+
+
+class BaseGeometry:
+    """This class serves as a base for geometry-related classes"""
+
+    def area(self):
+        """Placeholder method for calculating the area"""
+        
+        raise Exception("area() is not implemented")
