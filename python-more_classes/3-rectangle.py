1af0ae9dea38ae124f143ee66dff024de464a123
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 12:23:01 +0200

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -94,7 +94,7 @@ class Rectangle:
     def print(self):
         """Print the rectangle using the '#' character."""
 
-        i = 0, j = 0
+        i, j = 0
         if self.__height == 0 or self.__width == 0:
             return ""
         for i in range(self.__height):
