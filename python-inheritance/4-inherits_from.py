f9bba132048b2efca9b4b6343f6e9ad4576e4f93
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 17:18:41 +0200

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -14,4 +14,4 @@ def inherits_from(obj, a_class):
         bool: True if the object is an instance of a subclass of the specified class, False otherwise.
     """
     
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
