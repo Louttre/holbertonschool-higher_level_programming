bd76a8f027898a43932a422315484962ffaa6307
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 13:05:04 +0200

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """A class representing rectangle."""
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle instance.
