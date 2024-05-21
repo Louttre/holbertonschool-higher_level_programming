e9ea561e37f4a95df41de67e73efc25a5a011755
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-21 17:19:28 +0200

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,14 +4,16 @@
 
 def inherits_from(obj, a_class):
     """
-    Check if an object is an instance of a class that inherits from a specified class.
+    Check if an object is an instance 
+    of a class that inherits from a specified class.
 
     Args:
         obj: The object to be checked.
         a_class: The class to check against.
 
     Returns:
-        bool: True if the object is an instance of a subclass of the specified class, False otherwise.
+        bool: True if the object is an instance of a subclass
+        of the specified class, False otherwise.
     """
-    
+
     return issubclass(type(obj), a_class) and type(obj) is not a_class
