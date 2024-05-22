f039baeefddf3abf0e1600dd1d3fa68235ef4ac5
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-22 11:37:39 +0200

    Update 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,29 +1,6 @@
 #!/usr/bin/python3
 """Class definition for BaseGeometry"""
-
-
-class BaseGeometry:
-    """This class serves as a base for geometry-related classes"""
-
-    def area(self):
-        """Placeholder method for calculating the area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that a value is an integer greater than 0.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
