cbe493f5890fbc3b78dd22bdd2d95463e8cdb5f8
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 14:16:20 +0200

    Create 9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,31 @@
+ #!/usr/bin/python3
+
+
+class Student:
+    """
+    A class that defines a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        :param first_name: The first name of the student.
+        :param last_name: The last name of the student.
+        :param age: The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        :return: A dictionary containing the student's attributes.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
