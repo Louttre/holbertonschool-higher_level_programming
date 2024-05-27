9b34d8aa5fd87e86adfa2b80ce6a34954688bfaa
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 10:14:06 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,9 +7,7 @@ This module provides a function to read and print the content of a file.
 def read_file(filename=""):
     """
     This function reads the content of a specified file and prints it to the console.
-    
-    :param filename: The name of the file to be read. Defaults to an empty string.
     """
-    
+
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
