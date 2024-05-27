006281b6e1d5256010c7c10e3bb993d2f4dc66e5
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 13:32:57 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,18 @@
 import sys
 import json
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+def save_to_json_file(my_obj, filename):
+    """
+    Saves the given Python object to a JSON file.
+    """
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
+
+def load_from_json_file(filename):
+    """Load a JSON-formatted object from a file."""
+    with open(filename, "r") as file:
+        return json.load(file)
 
 try:
     data = load_from_json_file("add_item.json")
