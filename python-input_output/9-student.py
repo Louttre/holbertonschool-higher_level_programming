096b80bb1e132fe93cc62bdd95648dd9303d3ca4
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 14:52:17 +0200

    Update 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,26 +5,26 @@ Module
 
 
 class Student:
-      """
-      A class that defines a student with first name, last name, and age.
-      """
+    """
+    A class that defines a student with first name, last name, and age.
+    """
   
-      def __init__(self, first_name, last_name, age):
-          """
-          Initializes a new Student instance.
-  
-          :param first_name: The first name of the student.
-          :param last_name: The last name of the student.
-          :param age: The age of the student.
-          """
-          self.first_name = first_name
-          self.last_name = last_name
-          self.age = age
-  
-      def to_json(self):
-          """
-          Retrieves a dictionary representation of a Student instance.
-  
-          :return: A dictionary containing the student's attributes.
-          """
-          return self.__dict__
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+       :param first_name: The first name of the student.
+       :param last_name: The last name of the student.
+       :param age: The age of the student.
+       """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        :return: A dictionary containing the student's attributes.
+        """
+        return self.__dict__
