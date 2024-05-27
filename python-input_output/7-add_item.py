05f86ef5551a696166b488da7b23cc944640c619
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 13:44:45 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,13 +5,9 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_to_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-input = sys.argv[1:]
-
 try:
-    file_data = load_to_json_file("add_item.json")
+    data = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    file_data = []
-
-input_list = file_data + input
-
-save_to_json_file(input_list, "add_item.json")
+    data = []
+data += sys.argv[1:]
+save_to_json_file(data, "add_item.json")
