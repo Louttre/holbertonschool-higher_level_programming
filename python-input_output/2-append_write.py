66a28c568cd2d04f1d10f209c1adb0b679787853
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 10:49:34 +0200

    Create 2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module provides functionality to append text to an existing file. 
+If the file does not exist, it will create a new one and append the text to it.
+The module ensures that the file handling is done with UTF-8 encoding to support
+a wide range of text characters.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends the given text to the specified file.
+
+    Opens the file in append mode,
+    which allows text to be added to the end of the file
+    without truncating it. If the file does not exist, 
+    it creates a new file.
+
+    Parameters:
+    filename (str): The path to the file.
+    text (str): The text to be appended to the file.
+    """
+
+    with open(filename, 'a+', encoding="utf-8") as file:
+        file.write(text)
