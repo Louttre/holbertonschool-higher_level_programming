57bbd029da5cd442630daaabf79709c68876fc25
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 10:09:26 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,6 +11,6 @@ def read_file(filename=""):
     :param filename: The name of the file to be read. Defaults to an empty string.
     """
     
-    with open(filename, 'r', encoding='UTF-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
