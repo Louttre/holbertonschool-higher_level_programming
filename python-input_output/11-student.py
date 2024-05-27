1ba74c9fdf60094ad3a732110d7a82ae3209a843
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 15:45:00 +0200

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Student class."""
+
+
+class Student:
+    """
+    Defines a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        :param first_name: The first name of the student.
+        :param last_name: The last name of the student.
+        :param age: The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        :param attrs: List of attribute names to retrieve.
+        :return: A dictionary containing the specified attributes.
+        """
+        if attrs is None:
+            return vars(self)
+        else:
+            data = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    data[attr] = getattr(self, attr)
+            return data
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with those in the json dictionary.
+
+        :param json: A dictionary containing new attribute values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
