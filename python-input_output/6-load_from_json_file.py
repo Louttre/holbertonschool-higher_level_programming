9f110f40eafaeef48c7b781423b01df687c5ba46
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 13:20:35 +0200

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """Save the JSON string representation of object to file"""
     with open(filename, "r") as file:
-        json.load(file)
+        return json.load(file)
