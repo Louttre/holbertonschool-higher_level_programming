edf99a6d5e83668445ea1529b527087cd2a667fe
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-27 12:00:25 +0200

    Create 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+returns an object represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    returns an object represented by a JSON string.
+
+    :param my_obj: The object to be serialized to JSON.
+    :return: A JSON string representing the object.
+    """
+
+    return json.loads(my_obj)
