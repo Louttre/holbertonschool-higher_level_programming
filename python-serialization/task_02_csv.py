ff54d2e5ec8b7a7d4f36b6ff527e45a304890147
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-28 15:28:36 +0200

    Update task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -21,8 +21,7 @@ def convert_csv_to_json(filename):
             csv_reader = csv.DictReader(file)
         data = []
         for row in csv_reader:
-            rows = [row]
-            data.append(rows)
+            data.append(row)
         json_data = json.dumps(data)
         with open('data.json', 'w') as json_file:
             json_file.write(json_data)
