3dcf770450fa2a5c9257caf52869bd2cc9f94828
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-28 14:19:04 +0200

    Create task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import json
+import pickle
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def __str__(self):
+        return "Name: {}\nAge: {}\nIs Student: {}".format(self.name, self.age, self.is_student)
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self.__init__, file)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            loadata = CustomObject()
+            with open(filename, rb) as file:
+                loadata = pickle.load(filename)
+            return loadata
+        except (pickle.UnpicklingError, EOFError, Exception):
+            return None
