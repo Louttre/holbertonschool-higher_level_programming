d4c029eb2fc458a0222de4c61bf90d8062cf6898
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-28 15:09:02 +0200

    Update task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
-
+"""
+Module: csv, json.
+"""
 import csv
 import json
 
+
 def convert_csv_to_json(filename):
+    """
+    Convert a CSV file to JSON format.
+
+    Args:
+        filename (str): The path to the CSV file.
+
+    Returns:
+        bool: True if the conversion was successful, False otherwise.
+    """
     try:
         with open(filename, 'r') as file:
             csv_reader = csv.DictReader(file)
