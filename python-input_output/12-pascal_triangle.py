42621381325b279abd8bc749588a9cebfa598265
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-28 11:57:01 +0200

    Update 12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -19,7 +19,7 @@ def pascal_triangle(n):
     if n == 0:
         return 1
     basecase = [[1]]
-    for i in range(1, n + 1):
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(basecase[i - 1][j - 1] + basecase[i - 1][j])
