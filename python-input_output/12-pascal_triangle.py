0d76e67246fba12df2d1c94078eb86759f0f3138
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-28 11:53:29 +0200

    Create 12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module
+"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to the n-th row.
+    
+    Args:
+        n (int): The number of rows of Pascal's Triangle to generate.
+    
+    Returns:
+        list of list of int: A list of lists where each sublist represents a row in Pascal's Triangle.
+    """
+    if n < 0:
+        return [[]]
+    if n == 0:
+        return 1
+    basecase = [[1]]
+    for i in range(1, n + 1):
+        row = [1]
+        for j in range(1, i):
+            row.append(basecase[i - 1][i - 1] + basecase[i - 1][j])
+        row.append(1)
+        basecase.append(row)
+    return reseult
