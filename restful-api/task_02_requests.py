10a27c022a5d074461600c8f0f176ec57124ac4f
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-31 15:07:51 +0200

    Update task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,21 +4,23 @@ import csv
 
 
 def fetch_and_print_posts():
-    r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
     print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
-        posts = r.json()
+        posts = response .json()
         for post in posts:
             print(post['title'])
 
 def fetch_and_save_posts():
-    r = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if r.status_code == 200:
-        posts = r.json()
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    if response.status_code == 200:
+        posts = response.json()
         for post in posts:
-            data = [{"id": post["id"], "title": post["title"], "body": post["body"]}]
-        with open('posts.csv', 'w' ) as csvfile:
+            struc_data = [{"id": post["id"], "title": post["title"], "body": post["body"]}]
+        with open('posts.csv', 'w') as csvfile:
             fieldnames = ["id", "title", "body"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerows(data)
+            writer.writerows(struc_data)
