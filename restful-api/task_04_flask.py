f5802710d2733abb35c23df3ec66ca2556bf574a
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-31 14:54:38 +0200

    Update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
+from flask import Flask
+from flask import jsonify
+from flask import request
+import json
 
-from flask import Flask, jsonify, request
+
+users = {}
 
 app = Flask(__name__)
 
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}
-}
 
 @app.route("/")
 def home():
-    return "Welcome to the Flask API!"
+    message = "Welcome to the Flask API!"
+    return (message)
+
 
 @app.route("/data")
 def data():
     usernames = list(users.keys())
     return jsonify(usernames)
 
+
 @app.route("/status")
 def status():
     return "OK"
 
+
 @app.route("/users/<username>")
-def get_user(username):
-    user = users.get(username)
-    if user:
-        return jsonify(user)
+def get_users(username):
+    if username in users:
+        return jsonify(users[username])
     else:
         return jsonify({"error": "User not found"}), 404
 
+
 @app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.get_json()
-    username = data.get("username")
-    if username and username not in users:
-        users[username] = {
-            "username": data.get("username"),
-            "name": data.get("name"),
-            "age": data.get("age"),
-            "city": data.get("city")
-        }
-        return jsonify({"message": "User added", "user": users[username]}), 201
-    else:
+    if not request.json or "username" not in request.json:
         return jsonify({"error": "Username is required"}), 400
+    user_data = request.json
+    username = user_data["username"]
+    users[username] = {
+        "username": user_data.get("username"),
+        "name": user_data.get("name"),
+        "age": user_data.get("age"),
+        "city": user_data.get("city")
+    }
+    return jsonify({"message": "User added", "user": users[username]}), 201
+
 
 if __name__ == "__main__":
     app.run()
