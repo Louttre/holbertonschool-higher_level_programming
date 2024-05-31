a458499416cf43793c34f130244fa548562ceaf3
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-05-31 14:46:57 +0200

    Update task_03_http_server.py

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -41,7 +41,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 not found")
+            self.wfile.write(b"404 Not Found")
 
 PORT = 8000
 
