a97ef419a7dc45c55cd6e467d81eab0d9d2af512
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-19 11:26:59 +0200

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE {} ORDER BY id ASC;".format(sys.argv[4])
+    query = ("SELECT * FROM states WHERE"
+            " name LIKE {} ORDER BY id ASC;".format(sys.argv[4]))
     cursor.execute(query)
     query_rows = cursor.fetchall()
     for row in query_rows:
