530414461912877657fea553fc21078e71d05c27
root <seth.rueda@gmail.com>
2024-06-19 09:14:58 +0000

    add 1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Import the MySQLdb and sys module
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
