03c41f8eceb162218563895cd52f352ce123a5ce
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-20 14:01:45 +0200

    Update and rename model_state.py to 6-model_state.py

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('mysql+pymysql://root:root@localhost:3306/hbtn_0e_0_usa')
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class State(Base):
     __tablename__ = 'states'
@@ -10,10 +13,8 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
 
-engine = create_engine('mysql+pymysql://username:password@localhost:3306/database_name')
+
 Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
 new_state = State(name='California')
 session.add(new_state)
 session.commit()
