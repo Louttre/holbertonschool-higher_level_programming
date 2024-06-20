9535e90eb8c945f2002e6697c40a18769a4ac625
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-20 14:21:52 +0200

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,9 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class State(Base):
-"""state class"""
+    """state class"""
+
 
     __tablename__ = "states"
 
