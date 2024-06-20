ccefaa883c218d2ee68c87f7f2a54c2072bb9086
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-20 14:08:04 +0200

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+"""class definition of a State and an instance"""
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
 class State(Base):
+        """state class"""
     __tablename__ = 'states'
     
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
