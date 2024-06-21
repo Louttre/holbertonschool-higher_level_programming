9599530d3a180e7da493408596583492d9c7d67c
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-21 15:49:00 +0200

    Create model_city.py

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class definition of a State and an instance"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """state class"""
+
+    __tablename__ = "cities"
+
+    id = Column(
+                Integer,
+                primary_key=True,
+                autoincrement=True,
+                nullable=False,
+                unique=True
+                )
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
