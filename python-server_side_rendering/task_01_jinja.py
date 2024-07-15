52473727cdfe363ab0b82fd1d9c3a7a55ad6b752
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-15 15:03:46 +0200

    Update task_01_jinja.py

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -12,7 +12,7 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return render_template('/contact.html')
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
